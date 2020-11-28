a819d4b40c2af702cabd89fa8215c98aa1f7f2a7
Roy Jiny <royjiny@gmail.com>
2020-11-28 18:04:40 +0200

    start adding multi client

--- Game.py
+++ Game.py
@@ -1,9 +1,9 @@
 class Game:
-    def __init__(self, nA, nB, nC, player_socket):
+    def __init__(self, nA, nB, nC):
         self.nA = nA
         self.nB = nB
         self.nC = nC
-        self.socket = player_socket
+        self.winner = 0     # 1 is client, 2 is server
 
     def is_done(self):
         return self.nA == 0 and self.nB == 0 and self.nC == 0
@@ -15,22 +15,30 @@ class Game:
             else:
                 self.nC -= 1
         else:
-            if (self.nB > self.nC):
-                self.nB -= 1
+            if (self.nA > self.nC):
+                self.nA -= 1
             else:
                 self.nC -= 1
+        if self.is_done():
+            self.winner = 2
 
     def apply_client_turn(self,heap,amount):
         if heap == 'A':
             if self.nA >= amount:
                 self.nA -= amount
+                if self.is_done():
+                    self.winner = 1
                 return True
         if heap == 'B':
             if self.nB >= amount:
                 self.nB -= amount
+                if self.is_done():
+                    self.winner = 1
                 return True
         if heap == 'C':
             if self.nC >= amount:
                 self.nC -= amount
+                if self.is_done():
+                    self.winner = 1
                 return True
         return False
--- nim-server.py
+++ nim-server.py
@@ -2,6 +2,7 @@
 import socket
 import errno
 import sys
+import select
 from struct import *
 from communication import *
 from Game import *
@@ -11,69 +12,71 @@ nB = int(sys.argv[2])
 nC = int(sys.argv[3])
 max_num_of_players = int(sys.argv[4])
 size_of_wait_list = int(sys.argv[5])
-original_na = nA
-original_nb = nB
-original_nc = nC
 
 if len(sys.argv) > 6:
     server_port = int(sys.argv[6])
 else:
     server_port = 6444
 
-def run_game(conn_soc):
-    """
-    run a single game between server and client, until someone wins,
-    the client quits, or an error occures
-    """
-    game = Game(nA,nB,nC,conn_soc)
-    winner = 0
-    while True:
-        send_heaps_status(conn_soc,game)
-        if winner == 1:
-            send_char(conn_soc,'W')
-            break
-        elif winner == 2:
-            send_char(conn_soc, 'L')
-            break
-        else:
-            send_char(conn_soc, 'T')
-        
-        heap, num = receive_turn(conn_soc)
-        if (heap == 'Q'): break
+playing_clients = {}
 
-        is_legal = game.apply_client_turn(heap, num)
+def run_single_round(socket):
+    '''
+    run a single round for a single game
+    return true if the game is over, false if still going
+    '''
+    game = playing_clients[socket]
+    send_heaps_status(socket,game)
+    if game.winner == 1:
+        send_char(socket,'W')
+        return True
+    elif game.winner == 2:
+        send_char(socket, 'L')
+        return True
+    else:
+        send_char(socket, 'T')
+    
+    heap, num = receive_turn(socket)
+    if (heap == 'Q'): return True
 
-        if not is_legal:
-            send_char(conn_soc, 'I')
-        else:
-            send_char(conn_soc, 'A')
-        
-        if game.is_done():
-            winner = 1
-        else:
-            game.apply_server_turn()            
-            if game.is_done():
-                winner = 2
+    is_legal = game.apply_client_turn(heap, num)
 
-    conn_soc.close()
+    if not is_legal:
+        send_char(socket, 'I')
+    else:
+        send_char(socket, 'A')
+    return False
 
 try:
-    soc_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    soc_obj.bind(('', server_port))
-    soc_obj.listen(5)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('', server_port))
+    server.listen(5)
+    server.setblocking(0)
+
+    read_list = [server]
+
     while True:
         try:
-            print("Waiting for a new connection")
-            (conn_soc, address) = soc_obj.accept()
-            print("Received a new connection, starting game")
-            run_game(conn_soc)
+            print("run")
+            readable, _, _ = select.select(read_list,[],[],timeout=1)
+            for socket in readable:
+                if socket is server:
+                    print("server")
+                    new_client, _ = server.accept()
+                    read_list.append(new_client)
+                    new_game = Game(nA,nB,nC)
+                    playing_clients[new_client] = new_game
+                    is_over = run_single_round(new_client)
+                else:
+                    print("client")
+                    is_over = run_single_round(socket)
         except KeyboardInterrupt as error:
             print("Server was Stopped")
             break
         except Exception as err:
-            print("client disconnected with an error")
+            print("error")
             pass #client disconnected in the middle of communication, keep the server running 
-    soc_obj.close()
+    server.close()
 except OSError as error:
     if error.errno == errno.ECONNREFUSED:
         print("Failed to connect with client: connection refused by client")
